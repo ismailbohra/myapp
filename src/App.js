f075d6a6f9430f689e037007269a79ec1c0036ad
ismailbohra <ismailbohra99@gmail.com>
2024-10-31 19:06:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          My first project hosted on aws ec2 instance.
+        </p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
